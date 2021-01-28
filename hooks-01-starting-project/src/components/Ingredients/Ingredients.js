85328531e2f1d08a1259edd0cd6b70f271e99efb
JAI <t.jaisaichand4@gmail.com>
2021-01-28 22:23:06 +0530

    ...

--- hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,6 +7,31 @@ import IngredientList from './IngredientList';
 function Ingredients() {
 
   const [ingredientState, updateIngredientState] = useState([])
+  const [varOne, updatevarOne] = useState({ varOne: 'one' })
+  const [varTwo, updatevarTwo] = useState({ varTwo: 'two' })
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => updateIngredientState([json]))
+  }, [])
+
+  useEffect(() => {
+    console.log('varone')
+  }, [varOne])
+
+  useEffect(() => {
+    console.log('both')
+  }, [varOne, varTwo])
+
+
+  var onClickk = () => {
+    if (varOne.varOne == 'one') {
+      updatevarOne({ varOne: 'two' })
+    } else {
+      updatevarTwo({ varTwo: 'twooo' })
+    }
+  }
 
   const addIngredientHandler = (eve) => {
     console.log(eve);
@@ -22,7 +47,7 @@ function Ingredients() {
     console.log(eve);
     let gunn = [...ingredientState]
     gunn.forEach((val, ind) => {
-      if (val.id == eve) {
+      if (val.id === eve) {
         gunn.splice(ind, 1)
         updateIngredientState([
           ...gunn
@@ -35,6 +60,7 @@ function Ingredients() {
     <div className="App">
       <IngredientForm onUpdate={addIngredientHandler} />
 
+      <button onClick={onClickk}>clickk</button>
       <section>
         <Search />
         <IngredientList onRemoveItem={removeIngredientHandler} ingredients={ingredientState} />
